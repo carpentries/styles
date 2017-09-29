bb618bdedac32d5a86f0e2116e2d0fbd60520a57
David Mawdsley <david.mawdsley@manchester.ac.uk>
2017-09-29 08:52:55 +0100

    Use fswatch or inotifywait

--- bin/watchRmd.sh
+++ bin/watchRmd.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-fswatch --event=Updated _episodes_rmd/*.Rmd | make lesson-md
+
+if command -v fswatch >/dev/null 2>&1 ; then
+	watchcmd="fswatch --event Updated "
+elif command -v inotifywait >/dev/null 2>&1; then
+	watchcmd="inotifywait --format=%w -q -m -e close_write "
+else
+	echo "Cannot find fswatch or inotifywait"
+	exit 1
+fi
+echo $watchcmd
+$watchcmd _episodes_rmd/*.Rmd |xargs -n 1 Rscript -e "source('bin/generate_md_episodes.R')"  
 
