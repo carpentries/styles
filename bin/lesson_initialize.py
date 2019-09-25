8c4188ef8867bb88299ed3693274b27285a8f327
stamper <tbyhdgs@gmail.com>
2019-09-25 22:18:37 +0200

    Revert "adjust episode name in bin/lesson_initialize.py"
    This reverts commit b5af739cb2df57822207d1b21c9339c546cec3b3.

--- bin/lesson_initialize.py
+++ bin/lesson_initialize.py
@@ -14,7 +14,7 @@ BOILERPLATE = (
     'CONTRIBUTING.md',
     'README.md',
     '_config.yml',
-    '_episodes/introduction.md',
+    '_episodes/01-introduction.md',
     '_extras/about.md',
     '_extras/discuss.md',
     '_extras/figures.md',
