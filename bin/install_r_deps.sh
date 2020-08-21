85d4f0f4fe3c38678df09338f09a88f74bcad45e
François Michonneau <francois.michonneau@gmail.com>
2020-08-21 11:48:20 +0200

    clean up R scripts for dependency install

--- bin/install_r_deps.sh
+++ bin/install_r_deps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-Rscript -e "source(file.path('bin', 'dependencies.R')); install_dependencies(identify_dependencies())"
+Rscript -e "source(file.path('bin', 'dependencies.R')); install_required_packages(); install_dependencies(identify_dependencies())"
